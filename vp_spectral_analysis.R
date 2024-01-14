5bf95bdc8aa39000200576d47d8800849bb77b2f
Viktor Pavlov <viktor.pavlov@bolt.eu>
2024-01-14 14:00:09 +0200

    progress - add tunerR basics + real audio analysis

--- vp_spectral_analysis.R
+++ vp_spectral_analysis.R
@@ -1,3 +1,7 @@
+rm(list = ls())
+setwd("/Users/viktorpavlov/repos/school/iti8730-2023-dm-final-project")
+getwd()
+
 if (!requireNamespace("tuneR", quietly = TRUE)) {
   install.packages("tuneR")
 }
@@ -6,10 +10,14 @@ if (!requireNamespace("pastecs", quietly = TRUE)) {
   install.packages("pastecs")
 }
 library(pastecs)
+if (!requireNamespace("seewave", quietly = TRUE)) {
+  install.packages("seewave")
+}
+library(seewave)
 
 tuneR::setWavPlayer("/usr/bin/afplay")
 
-# tuneR Basiscs -----------------------------
+# tuneR Basiscs ------------------------------------------------------
 w_obj <- bind(sine(440), sine(220))
 show(w_obj)
 plot(w_obj)
@@ -46,3 +54,59 @@ qnotes <- quantize(snotes, w_spec_obj@energy, parts = 8)
 quantplot(qnotes, expected = rep(c(0, -12), each = 4), bars = 2)
 
 qlily <- quantMerge(snotes, 4, 4, 2)
+
+# analyze a real audio file - drum break -----------------------------
+drum_audio <- readWave("data/Motoko Break.wav")
+str(drum_audio)
+summary(drum_audio)
+
+plot(drum_audio)
+
+drum_audio_mono <- mono(drum_audio, "left")
+plot(drum_audio_mono)
+
+drum_spec_obj <- periodogram(
+  drum_audio_mono,
+  normalize = TRUE,
+  width = 1024,
+  overlap = 512
+)
+
+plot(drum_spec_obj, xlim = c(0, 2000), which = 1)
+image(drum_spec_obj, ylim = c(0, 1000))
+
+# analyze a real audio file - brass ----------------------------------
+brass_audio <- readWave("data/Brass_Waltzez.wav")
+str(brass_audio)
+summary(brass_audio)
+
+plot(brass_audio)
+
+brass_audio_mono <- mono(brass_audio, "left")
+plot(brass_audio_mono)
+
+brass_spec_obj <- periodogram(
+  brass_audio_mono,
+  normalize = TRUE,
+  width = 1024,
+  overlap = 512
+)
+
+plot(brass_spec_obj, xlim = c(0, 2000), which = 1)
+image(brass_spec_obj, ylim = c(0, 1000))
+
+ff <- FF(brass_spec_obj)
+print(ff)
+length(ff)
+summary(ff)
+
+notes <- noteFromFF(ff, 440)
+snotes <- smoother(notes)
+melodyplot(brass_spec_obj, snotes)
+
+qnotes <- quantize(snotes, brass_spec_obj@energy, parts = 8)
+quantplot(qnotes, expected = rep(c(0, -12), each = 4), bars = 2)
+
+# load GTZAN dataset -------------------------------------------------
+test_sound <- readWave("data/genres/blues/blues.00000.wav")
+summary(test_sound)
